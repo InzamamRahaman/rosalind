ff095116b88b609080efa1783def3e31cf78be97
IzzyRahaman <IzzyRahaman@users.noreply.github.com>
2014-03-10 22:22:37 -0400

    Added attempt at hamm

---
+++ Bioinformatics-Stronghold/hamm.fsx
@@ -0,0 +1,6 @@
+﻿
+let count pred = 
+    Seq.filter pred >> Seq.length
+let getHammingCode xs ys = 
+    Seq.zip xs ys |> count (fun (x, y) -> x <> y)
+getHammingCode "GAGCCTACTAACGGGAT" "CATCGTAATGACGGCCT"
