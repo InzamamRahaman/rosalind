89f0959d8449f412914731b5cead0720a2fb282c
IzzyRahaman <IzzyRahaman@users.noreply.github.com>
2014-03-16 23:21:08 -0400

    Added solution to gc

---
+++ Bioinformatics-Stronghold/Utilities.fs
@@ -0,0 +1,20 @@
+﻿module Utilities
+    
+    type FASTA = 
+        {Name : string; Sequence : string}
+
+    let parseDnaData (str : string) = 
+        (str.Split('\n'))
+        |> Array.toList
+        |> (fun xs -> {Name = List.head xs; Sequence = xs |> List.tail |> List.reduce (+)})
+
+    let parseAllData (str : string) = 
+        (str.Split('>'))
+        |> Array.filter ((<>) "")
+        |> Array.Parallel.map parseDnaData
+
+    let count pred xs = 
+        xs |> Seq.filter pred |> Seq.length
+
+   
+
---
+++ Bioinformatics-Stronghold/gc.fsx
@@ -0,0 +1,24 @@
+﻿#load "Utilities.fs"
+
+type FASTA = Utilities.FASTA
+
+let ($) f x = 
+    f x
+
+let fileContents = System.IO.File.ReadAllText "rosalind_gc.txt"
+let data = Utilities.parseAllData fileContents
+
+let getGCCount ({Name = n; Sequence = xs} : FASTA) = 
+    xs |> (Utilities.count (function | 'G' | 'C' -> true | _ -> false)) |> float
+
+
+let getGCContent x = 
+    (getGCCount x) / (float (Seq.length x.Sequence)) * 100.00
+
+let test = ({Name = "n"; Sequence = "AGCTATAG"} : FASTA) |> getGCContent
+
+
+let getMaxByGCContent (xs : FASTA []) = 
+    xs |> Array.maxBy getGCContent
+
+let ans = data |> getMaxByGCContent |> (fun x -> x.Name, (getGCContent x))
