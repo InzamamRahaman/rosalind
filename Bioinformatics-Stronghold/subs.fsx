ccbc0fa2fbd27f01fcbf99c2759ce4160aafdf21
IzzyRahaman <IzzyRahaman@users.noreply.github.com>
2014-03-22 21:34:03 -0400

    Solved subs

---
+++ Bioinformatics-Stronghold/subs.fsx
@@ -0,0 +1,21 @@
+﻿
+open System.Text.RegularExpressions
+
+let input = "GATATATGCATATACTT"
+let substring = "ATAT"
+let rx = new Regex(substring)
+
+let rec getAllLocations str (rx : Regex) start = 
+    let m = rx.Match(str, start)
+    if m.Success = false then
+        []
+    else
+        m.Index :: (getAllLocations str rx (m.Index + 1))
+
+getAllLocations input rx 0
+|> List.map ((+) 1)
+|> List.iter (printfn "%d")
+        
+    
+
+
