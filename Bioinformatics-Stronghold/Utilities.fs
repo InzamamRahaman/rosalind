2168e8b1ef5a3f3f297c49331e83a2fb3ed5516c
IzzyRahaman <IzzyRahaman@users.noreply.github.com>
2014-03-22 20:56:32 -0400

    Solved prot
    Added code to solve prot.
    Added code to parse a codon mapping give in the format at rosalind,
    added code to perform a map partial operation, and added code to split a
    sequence into several sequences of a specified size comprising
    contiguous elements of the original sequence

--- Bioinformatics-Stronghold/Utilities.fs
+++ Bioinformatics-Stronghold/Utilities.fs
@@ -16,5 +16,29 @@
     let count pred xs = 
         xs |> Seq.filter pred |> Seq.length
 
+    let cartesianProduct s1 s2 = 
+        s1 |> Seq.collect (fun x -> s2 |> Seq.map (fun y -> x, y))
+
+    let rec splitIntoSizedSeq n xs = 
+        seq {
+            
+            if Seq.isEmpty xs = false && Seq.length xs >= n then
+                yield (xs |> Seq.take n)
+                yield! (splitIntoSizedSeq n (Seq.skip n xs))
+        }
+
+    let parseCodonMap (str : string) = 
+        str.Split([|' ' ;'\n'; '\r'|])
+        |> Array.filter ((<>) "") 
+        |> splitIntoSizedSeq 2
+        |> Seq.map (fun s -> (Seq.head s, Seq.head (Seq.skip 1 s)))
+        |> Map.ofSeq
+
+    let mapPartial f xs = 
+        xs
+        |> Seq.map f 
+        |> Seq.filter ((<>) None)
+        |> Seq.map (function | (Some x) -> x)
+
    
 
---
+++ Bioinformatics-Stronghold/prot.fsx
@@ -0,0 +1,21 @@
+﻿
+#load "Utilities.fs"
+
+let codonTable = 
+    System.IO.File.ReadAllText "codon_table.txt" 
+    |> Utilities.parseCodonMap
+
+
+let input = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
+
+let ans =
+    input 
+    |> Utilities.splitIntoSizedSeq 3 
+    |> Seq.map (Seq.map (fun ch -> ch.ToString()))
+    |> Seq.map (Seq.reduce (+))
+    |> Utilities.mapPartial (fun str -> Map.tryFind str codonTable)
+    |> Seq.takeWhile ((<>) "Stop")
+    |> Seq.fold (+) ""
+
+
+
